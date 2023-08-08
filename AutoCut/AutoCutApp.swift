37a59a09319e05927d8866683926e6da0d9ee8da
Juan Lucas Umali <juanlucasumali@gmail.com>
2023-08-08 00:41:05 -0700

    Initial Commit

---
+++ AutoCut/AutoCutApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoCutApp.swift
+//  AutoCut
+//
+//  Created by Juan Lucas Umali on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct AutoCutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
